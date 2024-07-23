6c28a96718a9c0647f2fe426f012452a4f37873c
LtheBest <loicstonda@gmail.com>
2024-07-23 11:00:10 +0200

    update

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,12 @@
 const request = require('supertest');
 const app = require('../app');
+const server = app.listen(3000); 
 
 describe('GET /search', () => {
+    afterAll(done => {
+        server.close(done);
+    });
+
     it('should return filtered items based on query', async () => {
         const response = await request(app).get('/search?q=item1');
         expect(response.status).toBe(200);
