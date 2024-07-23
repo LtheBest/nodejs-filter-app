62ccf87026679c2f39b8d553d98a4bdeb7815f7f
LtheBest <loicstonda@gmail.com>
2024-07-23 10:44:17 +0200

    correction lint

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ app.use(express.json());
 const items = [
     { id: 1, name: 'item1' },
     { id: 2, name: 'item2' },
-    { id: 3, name: 'item3' }
+    { id: 3, name: 'item3' },
+    { id: 4, name: 'item4' }
 ];
 
 app.get('/search', (req, res) => {
