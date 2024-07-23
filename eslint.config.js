2066dff907f0919d3f5de3a0855e7f280bd2398c
LtheBest <loicstonda@gmail.com>
2024-07-23 10:34:53 +0200

    replace files

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,19 @@
 // eslint.config.js
 export default [
     {
-        files: ['**/*.js'],
-        languageOptions: {
-            ecmaVersion: 2021,
-            sourceType: 'module',
-            globals: {
-                browser: true,
-                node: true,
-                jest: true,
-            },
+      files: ["**/*.js"],
+      languageOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        globals: {
+          browser: true,
+          node: true,
+          jest: true,
         },
-        rules: {
-            'quotes': ['error', 'single'],
-        },
+      },
+      rules: {
+        quotes: ["error", "single"],
+      },
     },
-];
+  ];
+  
